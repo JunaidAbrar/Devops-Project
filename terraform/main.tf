aa0729f2e15aab3bad64e1d515e25349d0f5ddeb
Junaid Abrar Razeen <junaid.abrar.razeen@gmail.com>
2024-08-24 02:22:20 +0600

    infra plan 4

--- terraform/main.tf
+++ terraform/main.tf
@@ -78,10 +78,11 @@ resource "aws_security_group" "maingoup" {
 
 
 resource "aws_key_pair" "deployer"{
-    key_name = var.key_name
-    public_key = var.public_key
+    key_name   = var.key_name
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDCHnkMaUmdnhyuJ7qKZREQmoT6qUvf2MvA1HqCpiMAFaQnjalx9etqHgpfE7yahypy1YyiL9/cOsUdUfsaD6eBvHQ3ko761E6Md5MrdR1Xhl/8arE3HfwgkMOWYLtNlfI1qzE8EBfVstk9SdlCgOvcm/91evpAqD7OuzbdamWjw0TeZJP2d5vcM6CijrpkMIcRjlZ6jvrdrk6SOighI23WQbufHdYk5E9cTRlf9Az88qnobTvZ7P+Lf191d3RIhH4cfptOLp1H4qgjaYJ2ZKKu93RlBu6croCV9z//Nzr7XEjMJT3h9oIC8KYLcfkhjuJMEhEbQIQEz8xHmOi2N3bpdgLc1n7DmMYibstJHlyzt8ztHJSxBTZY/I5s1BtEHTzgUAVXG4w5EmIlcQ62Dt0AEwJ/1lrqp47+03Ov9/DesvkSe6NlFW/0GZOyvnSc7H7E7KW1KSy7FD3tybQn59LpNNcenHyeSsc2yYbruje7MLdk+Ih0BgJnKCVpzr6f7RM= junu@abrar-21141023"
 }
-output "instance_public_ip"{
-    value = aws_instance.server.public_ip
+
+output "instance_public_ip" {
+    value     = aws_instance.server.public_ip
     sensitive = true
 }
