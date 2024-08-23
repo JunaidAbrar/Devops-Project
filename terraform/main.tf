149b891d85f0e551613a9f4872e1e112439d041a
Junaid Abrar Razeen <junaid.abrar.razeen@gmail.com>
2024-08-24 00:56:14 +0600

    infra plan

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,9 +18,9 @@ resource "aws_instance" "server" {
     instance_type = "t2.micro"
     key_name = aws_key_pair.deployer.key_name
     vpc_security_group_ids = [aws_security_group.maingoup.id]
-    iam_instance_profile = aws_iam_instance_profile.ec2-profile-name
+    iam_instance_profile = aws_iam_instance_profile.ec2-profile.name
     connection {
-        type "ssh"
+        type = "ssh"
         host = self.public_ip
         user = "ubuntu"
         private_key = var.private_key
